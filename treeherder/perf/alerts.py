572e041aeceff78109aed31309f0a300664cb4d2
Ionut Goldan <gold.ionut@gmail.com>
2021-01-20 15:25:39 +0200

    WIP Simulate alert generation

--- treeherder/perf/alerts.py
+++ treeherder/perf/alerts.py
@@ -11,6 +11,7 @@ from django.db import transaction
 from django.db.models import F, Q
 from django.db.models.query import QuerySet
 
+from treeherder.model.models import Push
 from treeherder.perf.exceptions import MissingRecords
 from treeherder.perf.models import (
     BackfillRecord,
@@ -22,6 +23,8 @@ from treeherder.perf.models import (
 )
 from treeherder.perfalert.perfalert import RevisionDatum, detect_changes
 from treeherder.utils import default_serializer
+
+logger = logging.getLogger(__name__)
 
 
 def get_alert_properties(prev_value, new_value, lower_is_better):
@@ -136,6 +139,125 @@ def generate_new_alerts_in_series(signature):
                         't_value': t_value,
                     },
                 )
+
+
+# TODO: DRY this
+def simulate_alert_generation(signature: PerformanceSignature) -> None:
+    # get series data starting from either:
+    # (1) the last alert, if there is one
+    # (2) the alerts max age
+    # (use whichever is newer)
+    logger.info(
+        f'Simulating alert generation for signature ID {signature.id}'
+        f'({signature.repository.name} repository, {signature.framework.name} framework)...'
+    )
+    max_alert_age = datetime.now() - settings.PERFHERDER_ALERTS_MAX_AGE
+    series = PerformanceDatum.objects.filter(signature=signature, push_timestamp__gte=max_alert_age)
+    latest_alert_timestamp = (
+        PerformanceAlert.objects.filter(series_signature=signature)
+        .select_related('summary__push__time')
+        .order_by('-summary__push__time')
+        .values_list('summary__push__time', flat=True)[:1]
+    )
+    if latest_alert_timestamp:
+        series = series.filter(push_timestamp__gt=latest_alert_timestamp[0])
+
+    revision_data = {}
+    for data_point in series:
+        if not revision_data.get(data_point.push.revision):
+            revision_data[data_point.push.revision] = RevisionDatum(
+                int(time.mktime(data_point.push_timestamp.timetuple())),
+                data_point.push_id,
+                [],
+                str(data_point.push_timestamp),
+            )
+        revision_data[data_point.push.revision].values.append(data_point.value)
+    from pprint import pprint
+
+    pprint(revision_data)
+
+    min_back_window = signature.min_back_window
+    if min_back_window is None:
+        min_back_window = settings.PERFHERDER_ALERTS_MIN_BACK_WINDOW
+    max_back_window = signature.max_back_window
+    if max_back_window is None:
+        max_back_window = settings.PERFHERDER_ALERTS_MAX_BACK_WINDOW
+    fore_window = signature.fore_window
+    if fore_window is None:
+        fore_window = settings.PERFHERDER_ALERTS_FORE_WINDOW
+    alert_threshold = signature.alert_threshold
+    if alert_threshold is None:
+        alert_threshold = settings.PERFHERDER_REGRESSION_THRESHOLD
+
+    analyzed_series = detect_changes(
+        revision_data.values(),
+        min_back_window=min_back_window,
+        max_back_window=max_back_window,
+        fore_window=fore_window,
+    )
+    no_change_detected = True
+    for (prev, cur) in zip(analyzed_series, analyzed_series[1:]):
+        if cur.change_detected:
+            logger.info('Some change is noticeable')
+            prev_value = cur.historical_stats['avg']
+            new_value = cur.forward_stats['avg']
+            alert_properties = get_alert_properties(
+                prev_value, new_value, signature.lower_is_better
+            )
+
+            # ignore regressions below the configured regression
+            # threshold
+            if (
+                (
+                    signature.alert_change_type is None
+                    or signature.alert_change_type == PerformanceSignature.ALERT_PCT
+                )
+                and alert_properties.pct_change < alert_threshold
+            ) or (
+                signature.alert_change_type == PerformanceSignature.ALERT_ABS
+                and alert_properties.delta < alert_threshold
+            ):
+                continue
+
+            no_change_detected = False
+            push = Push.objects.get(cur.push_id)
+            prev_push = Push.objects.get(cur.prev_push_id)
+            logger.info(
+                f'Detected performance change on suspect range '
+                f'{push.revision}..{prev_push.revision}'
+            )
+            # summary, _ = PerformanceAlertSummary.objects.get_or_create(
+            #     repository=signature.repository,
+            #     framework=signature.framework,
+            #     push_id=cur.push_id,
+            #     prev_push_id=prev.push_id,
+            #     defaults={
+            #         'manually_created': False,
+            #         'created': datetime.utcfromtimestamp(cur.push_timestamp),
+            #     },
+            # )
+
+            # django/mysql doesn't understand "inf", so just use some
+            # arbitrarily high value for that case
+            t_value = cur.t
+            if t_value == float('inf'):
+                t_value = 1000
+            logger.info(f'Creating alert {alert_properties}')
+            # PerformanceAlert.objects.update_or_create(
+            #     summary=summary,
+            #     series_signature=signature,
+            #     defaults={
+            #         'is_regression': alert_properties.is_regression,
+            #         'amount_pct': alert_properties.pct_change,
+            #         'amount_abs': alert_properties.delta,
+            #         'prev_value': prev_value,
+            #         'new_value': new_value,
+            #         't_value': t_value,
+            #     },
+            # )
+
+    if no_change_detected:
+        logger.info('No performance change detected')
 
 
 class AlertsPicker:
